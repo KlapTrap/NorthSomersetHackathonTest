0794770b48b72e4128ff3bf72e5458974539a463
Nathan Jones <nathanmjones1@gmail.com>
2015-06-12 15:53:53 +0100

    Added not found modals

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,14 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('nsTrial', ['ionic', 'nsTrial.controllers', 'nsTrial.services', 'nsTrial.directives', 'leaflet-directive'])
+angular.module('nsTrial', ['ionic', 'nsTrial.controllers', 'nsTrial.services', 'nsTrial.directives', 'leaflet-directive', 'ngCordova'])
 
   .run(function ($ionicPlatform) {
+
+    //$cordovaStatusBar.overlaysWebView(true);
+    //
+    //$cordovaStatusBar.style(1); //Light
+
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -15,7 +20,8 @@ angular.module('nsTrial', ['ionic', 'nsTrial.controllers', 'nsTrial.services', '
       }
       if (window.StatusBar) {
         // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
+        //StatusBar.styleDefault();
+        StatusBar.style(1);
       }
     });
   })
@@ -38,7 +44,7 @@ angular.module('nsTrial', ['ionic', 'nsTrial.controllers', 'nsTrial.services', '
       }).state('app.map', {
         url: "/map",
         resolve: {
-          historicalImages: function(nsImagesService){
+          historicalImages: function (nsImagesService) {
             return nsImagesService.getData();
           }
         },
--- app/scripts/map.controller.js
+++ app/scripts/map.controller.js
@@ -1,9 +1,8 @@
 'use strict';
 angular.module('nsTrial.controllers')
-  .controller('MapCtrl', function ($scope, historicalImages, nsImagesService, nsUserDataService) {
+  .controller('MapCtrl', function ($scope, historicalImages, nsImagesService, nsUserDataService, $ionicModal) {
     $scope.defaults = {
       tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      maxZoom: 14,
       path: {
         weight: 10,
         color: '#800000',
@@ -11,21 +10,71 @@ angular.module('nsTrial.controllers')
       }
     };
 
+    var markerIcons = {
+      notFoundIcon: {
+        type: 'awesomeMarker',
+        icon: 'question',
+        prefix: 'fa',
+        markerColor: 'grey'
+      },
+      foundIcon: {
+        type: 'awesomeMarker',
+        icon: 'tick',
+        prefix: 'fa',
+        markerColor: 'green'
+      },
+      meIcon: {
+        type: 'awesomeMarker',
+        icon: 'dot-circle-o',
+        prefix: 'fa',
+        markerColor: 'blue'
+      }
+    };
+
     $scope.center = {
       lat: 51.353906,
       lng: -2.983046,
-      zoom: 30
+      zoom: 14
     };
 
-    $scope.$on('leafletDirectiveMarker.click', function(e, args) {
-      console.log(e);
-      console.log("Leaflet Click");
+    $scope.$on('leafletDirectiveMarker.click', function (e, args) {
+      if (!args.model || args.model.me) {
+        // This is the user marker
+        return;
+      }
+      var template = 'templates/notFoundModal.html';
+      var scope = $scope.$new(true);
+      scope.model = args.model;
+
+      if (args.model.metaData.found) {
+        template = 'templates/foundModal.html';
+      }
+      $ionicModal.fromTemplateUrl(template, {
+        scope: scope,
+        animation: 'slide-in-up'
+      }).then(function (modal) {
+        scope.modal = modal;
+        scope.modal.show();
+        scope.close = function () {
+          scope.modal.hide();
+        }
+      });
+      // Make sure we clean up
+      scope.$on('modal.hidden', function () {
+        scope.$destroy();
+      });
+      scope.$on('$destroy', function () {
+        scope.modal.remove();
+      });
+
     });
 
     function setMyPositionOnMap(position) {
       $scope.markers.me = {
         lat: position.coords.latitude,
-        lng: position.coords.longitude
+        lng: position.coords.longitude,
+        icon: markerIcons.meIcon,
+        me: true
       };
     }
 
@@ -34,12 +83,14 @@ angular.module('nsTrial.controllers')
       for (var i = 0; i < historicalImages.length; i++) {
         var point = historicalImages[i];
         var ref = point[nsImagesService.CONSTANTS.REF];
+        var found = nsUserDataService.isPointFound(point);
         $scope.markers[ref] = {
           lat: parseFloat(point[nsImagesService.CONSTANTS.LAT]),
           lng: parseFloat(point[nsImagesService.CONSTANTS.LNG]),
-          message: point[nsImagesService.CONSTANTS.DESC],
+          icon: found ? markerIcons.foundIcon : markerIcons.notFoundIcon,
           metaData: {
-            found: nsUserDataService.isPointFound(point)
+            found: found,
+            full: point
           }
         };
       }
@@ -49,7 +100,13 @@ angular.module('nsTrial.controllers')
 
     navigator.geolocation.getCurrentPosition(function (position) {
       setMyPositionOnMap(position);
+      $scope.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        zoom: 14
+      };
     });
+
     navigator.geolocation.watchPosition(function (position) {
       setMyPositionOnMap(position);
     });
